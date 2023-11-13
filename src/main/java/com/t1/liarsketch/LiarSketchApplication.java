a6eedfca8c25623bc7473f2d528f7949c5615a9d
yh <azss0470@gmail.com>
2023-11-13 16:18:45 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/t1/liarsketch/LiarSketchApplication.java
@@ -0,0 +1,15 @@
+package com.t1.liarsketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiarSketchApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World :)");
+        SpringApplication.run(LiarSketchApplication.class, args);
+
+    }
+
+}
